19d4cc8f5e8070443a0a22ef65918acc0702ca34
M763468 <124549499+M763468@users.noreply.github.com>
2026-06-06 13:34:36 +0900

    Use real MMR OCR engine in replacement test

--- tests/test_pipeline_numbering_mmr_provider.py
+++ tests/test_pipeline_numbering_mmr_provider.py
@@ -1,8 +1,5 @@
+from src.measure_numbering.mmr import MMROCREngine
 from src.pipeline.steps import numbering
-
-
-class MMROCREngine:
-    pass
 
 
 class CustomInjectedEngine:
@@ -13,7 +10,7 @@ def test_should_replace_absent_engine():
     assert numbering._should_replace_mmr_ocr_engine(None)
 
 
-def test_should_replace_default_named_engine():
+def test_should_replace_default_engine_by_exact_type():
     assert numbering._should_replace_mmr_ocr_engine(MMROCREngine())
 
 
